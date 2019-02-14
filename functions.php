53cd48ee75280c899f60605d81075467b87b3442
Thors97 <100346821@alumnos.uc3m.es>
2019-02-14 20:55:35 +0100

    Ya va

--- functions.php
+++ functions.php
@@ -1,11 +1,15 @@
 
 <?php
+
+// Esta función nos permite conctarnos al servidor de ldap que queremos.
+
 function conect_ldap($user , $passwd){
 
   $ds = ldap_connect ("ldaps://repldap.lab.it.uc3m.es",636) //Nos conectamos al servidor de ldap
   or die ("Could not connect to LDAP Server");
   $ldaprdn = "uid=".$user.",ou=Alum,dc=lab,dc=it,dc=uc3m,dc=es";
   $ldappass = $passwd;
+  ldap_set_option($ds, LDAP_OPT_PROTOCOL_VERSION, 3);
 
     return array ($ds , $ldaprdn , $ldappass);
 
